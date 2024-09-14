98fa4a80428e237a7ab4c939d8fdd6221aed0d57
Wydy <1937986+wydy@users.noreply.github.com>
2024-09-15 05:38:12 +0800

    Update FileSystemManager.tsx

--- apps/website/src/components/Playground/Workspace/FileSystemManager.tsx
+++ apps/website/src/components/Playground/Workspace/FileSystemManager.tsx
@@ -122,7 +122,7 @@ export default function FileSystemManager({
                     <UploadIcon fontSize="small" />
                   </IconButton>
                 </Tooltip>
-                /*<Tooltip title="Create a new folder">
+                <!--<Tooltip title="Create a new folder">
                   <IconButton
                     onClick={() => {
                       setDirName("");
@@ -133,7 +133,7 @@ export default function FileSystemManager({
                   >
                     <CreateNewFolderIcon />
                   </IconButton>
-                </Tooltip>*/
+                </Tooltip>-->
                 <Tooltip title="Refresh directory">
                   <IconButton
                     onClick={onRefresh}
@@ -145,7 +145,7 @@ export default function FileSystemManager({
                 </Tooltip>
               </Box>
             </Stack>
-            /*<Typography>{`Path: ${path}`}</Typography>*/
+            <!--<Typography>{`Path: ${path}`}</Typography>-->
             <List style={{ height: 480, overflowX: "auto" }}>
               {nodes.map(({ name, isDir }, index) =>
                 isDir ? (
